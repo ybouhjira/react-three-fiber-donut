a54c994db7fe72820c7027e1cf58ae7155678af5
Youssef Bouhjira <bouhjira.youssef@gmail.com>
2023-04-04 23:11:29 +0000

    load fluid simulation data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import React from "react";
-import { applyProps, Canvas, MeshProps } from "@react-three/fiber";
+import { applyProps, Canvas, MeshProps, useLoader } from "@react-three/fiber";
 import {
   AccumulativeShadows,
   Environment,
@@ -10,34 +10,41 @@ import {
 } from "@react-three/drei";
 import { Mesh } from "three";
 import { color } from "three/examples/jsm/nodes/shadernode/ShaderNodeBaseElements";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
+import { FluidAnimation } from "./FluidAnimation";
 
 function App() {
+  const obj = useLoader(OBJLoader, "../public/fluid/fluid_mesh_0025.obj");
+
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
       <Canvas shadows camera={{ position: [0, 5, 5] }}>
+        <primitive object={obj} position={[5, 5, 5]} />
+        <FluidAnimation />
         <color attach="background" args={["goldenrod"]} />
         <Environment preset="city" />
         <Donut scale={[1, 1, 1]} position={[0, 0.4, 0]} rotation={[0, 0, 0]} />
-        <AccumulativeShadows
-          temporal
-          frames={100}
-          color="goldenrod"
-          alphaTest={0.65}
-          opacity={2}
-          scale={14}
-          position={[0, 0, 0]}
-        >
-          <RandomizedLight
-            amount={8}
-            radius={4}
-            ambient={0.5}
-            bias={0.001}
-            position={[5, 5, -10]}
-          />
-        </AccumulativeShadows>
+        {/*<AccumulativeShadows*/}
+        {/*  temporal*/}
+        {/*  frames={100}*/}
+        {/*  color="goldenrod"*/}
+        {/*  alphaTest={0.65}*/}
+        {/*  opacity={2}*/}
+        {/*  scale={14}*/}
+        {/*  position={[0, 0, 0]}*/}
+        {/*>*/}
+        {/*  <RandomizedLight*/}
+        {/*    amount={8}*/}
+        {/*    radius={4}*/}
+        {/*    ambient={0.5}*/}
+        {/*    bias={0.001}*/}
+        {/*    position={[5, 5, -10]}*/}
+        {/*  />*/}
+        {/*</AccumulativeShadows>*/}
         <OrbitControls
           enablePan={false}
-          enableZoom={false}
+          /*enableZoom={false}
+           */
         />
       </Canvas>
     </div>
